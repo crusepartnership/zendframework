4e42a47888be8c74670a658ba46a4f1800a0f4f8
Tristan Kenney <tristan@kenney.co>
2015-11-03 19:19:41 +1100

    Modified Plugin loader to use composer

--- library/Zend/Loader/PluginLoader.php
+++ library/Zend/Loader/PluginLoader.php
@@ -289,7 +289,7 @@ class Zend_Loader_PluginLoader implements Zend_Loader_PluginLoader_Interface
     public function getClassName($name)
     {
         $name = $this->_formatName($name);
-        if ($this->_useStaticRegistry 
+        if ($this->_useStaticRegistry
             && isset(self::$_staticLoadedPlugins[$this->_useStaticRegistry][$name])
         ) {
             return self::$_staticLoadedPlugins[$this->_useStaticRegistry][$name];
@@ -302,14 +302,14 @@ class Zend_Loader_PluginLoader implements Zend_Loader_PluginLoader_Interface
 
     /**
      * Get path to plugin class
-     * 
-     * @param  mixed $name 
+     *
+     * @param  mixed $name
      * @return string|false False if not found
      */
     public function getClassPath($name)
     {
         $name = $this->_formatName($name);
-        if ($this->_useStaticRegistry 
+        if ($this->_useStaticRegistry
             && !empty(self::$_staticLoadedPluginPaths[$this->_useStaticRegistry][$name])
         ) {
             return self::$_staticLoadedPluginPaths[$this->_useStaticRegistry][$name];
@@ -359,7 +359,7 @@ class Zend_Loader_PluginLoader implements Zend_Loader_PluginLoader_Interface
         foreach ($registry as $prefix => $paths) {
             $className = $prefix . $name;
 
-            if (class_exists($className, false)) {
+            if (class_exists($className)) {
                 $found = true;
                 break;
             }
@@ -403,10 +403,10 @@ class Zend_Loader_PluginLoader implements Zend_Loader_PluginLoader_Interface
     /**
      * Set path to class file cache
      *
-     * Specify a path to a file that will add include_once statements for each 
+     * Specify a path to a file that will add include_once statements for each
      * plugin class loaded. This is an opt-in feature for performance purposes.
-     * 
-     * @param  string $file 
+     *
+     * @param  string $file
      * @return void
      * @throws Zend_Loader_PluginLoader_Exception if file is not writeable or path does not exist
      */
@@ -435,7 +435,7 @@ class Zend_Loader_PluginLoader implements Zend_Loader_PluginLoader_Interface
 
     /**
      * Retrieve class file cache path
-     * 
+     *
      * @return string|null
      */
     public static function getIncludeFileCache()
@@ -445,8 +445,8 @@ class Zend_Loader_PluginLoader implements Zend_Loader_PluginLoader_Interface
 
     /**
      * Append an include_once statement to the class file cache
-     * 
-     * @param  string $incFile 
+     *
+     * @param  string $incFile
      * @return void
      */
     protected static function _appendIncFile($incFile)
